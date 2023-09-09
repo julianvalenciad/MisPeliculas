2976a2a0d97c01864c3d2083a5ead5d467e7f9ae
Julian Valencia <julian.valenciad@udea.edu.co>
2023-09-09 00:27:52 -0500

    Se finaliza el segundo punto

--- app/src/main/java/com/julianvalencia/mispeliculas/RegisterActivity.kt
+++ app/src/main/java/com/julianvalencia/mispeliculas/RegisterActivity.kt
@@ -1,16 +1,21 @@
 package com.julianvalencia.mispeliculas
 
+import android.app.DatePickerDialog
+import android.icu.text.SimpleDateFormat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.julianvalencia.mispeliculas.databinding.ActivityRegisterBinding
+import java.util.Calendar
+import java.util.Locale
 
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var registerBinding: ActivityRegisterBinding
-
+    private var cal = Calendar.getInstance()
+    private var FechaNacimiento:String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         registerBinding = ActivityRegisterBinding.inflate(layoutInflater)
@@ -22,20 +27,51 @@ class RegisterActivity : AppCompatActivity() {
             val email = registerBinding.emailEditText.text.toString()
             val password = registerBinding.passwordEditText.text.toString()
             val repPassword = registerBinding.repPasswordEditText.text.toString()
+            val ciudades = registerBinding.spinnerCity.selectedItem.toString()
             var genero = "Masculino"
             if (registerBinding.femaleRadioButton.isChecked){
                 genero = "Femenino"
             }
 
-            if (password == repPassword){
-                val info = email + "\n" +password + "\n" + genero
-                registerBinding.infoTextView.text = info
-            } else{
-                Toast.makeText(this,"Las contraseñas no son iguales", Toast.LENGTH_LONG).show()
+            if (email == "" || password == "" || genero == "" || ciudades == "" || FechaNacimiento == "") {
+                Toast.makeText(this, "Falta ingresar algún elemento", Toast.LENGTH_LONG).show()
+            }else {
+                if (password == repPassword) {
+                    val info = email + "\n" + password + "\n" + genero + "\n" + ciudades + "\n" + FechaNacimiento
+                    registerBinding.infoTextView.text = info
+                } else{
+                    Toast.makeText(this,"Las contraseñas no son iguales", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
+        val dateSetListener =
+            DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, month)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+                val format = "MM/dd/yyyy"
+                val sdf = SimpleDateFormat(format, Locale.US)
+                FechaNacimiento = sdf.format(cal.time).toString()
+                registerBinding.datebutton.setText(FechaNacimiento)
             }
 
-
-
+        registerBinding.datebutton.setOnClickListener {
+            DatePickerDialog(
+                this,
+                dateSetListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)
+            ).show()
         }
+
+
+
+
+
+
     }
+
 }
